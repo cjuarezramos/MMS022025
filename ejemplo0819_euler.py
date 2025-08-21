4fbf6c0e5d4b653173e07b9f09cf57ed7c71680a
Carlos Juárez <cjuarez@uca.edu.sv>
2025-08-21 11:41:29 -0600

    ejemplo de euler actualizado el 21 de agosto

--- ejemplo0819_euler.py
+++ ejemplo0819_euler.py
@@ -5,19 +5,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 # DAtos del sistema
-M = 100 # Masa del sistema kg
+M = 10 # Masa del sistema kg
 B = 0.1 # Coeficiente de friccion kg/s
 # Condiciones iniciales
-v0 = 10 # Velocidad inicial m/s
+v0 = 50 # Velocidad inicial m/s
 # Tiempo de simulacion
 t0 = 0 # s
-tf = 5000 # s
-N = 1000 # Numero de pasos
+tf = 500 # s
+N = 10 # Numero de intervalos
 # Vector de tiempo
 t = np.linspace(t0, tf, N+1)
 # Vector de velocidad
 v_analitica = v0 * np.exp(-B/M * t)
-# graficando
+'''# graficando
 plt.plot(t, v_analitica, label='Analitica', color='blue')
 plt.xlabel('Tiempo (s)')
 plt.ylabel('Velocidad (m/s)')
@@ -25,3 +25,27 @@ plt.title('Velocidad del sistema con friccion')
 plt.grid()
 plt.legend()
 plt.show()  
+'''
+# Solución por métodos numéricos
+# Paso de tiempo
+h = (tf - t0) / N
+# Inicializando arreglos para almacenar la solución
+v_euler = np.zeros(N+1)
+v_euler[0] = v0  # Condición inicial
+# método de euler
+for i in range(N):
+    v_euler[i+1] = v_euler[i] + h * (-B/M * v_euler[i])
+    print(v_euler)
+#Gráficos
+plt.plot(t, v_analitica, label='Analitica', color='blue')
+plt.plot(t, v_euler, label='Euler', color='red')
+plt.xlabel('Tiempo (s)')
+plt.ylabel('Velocidad (m/s)')
+plt.title('Velocidad del sistema con friccion')
+plt.grid()
+plt.legend()
+plt.show()  
+
+
+
+
