25ce9b230f338f2d52d08ba61fa33c142b0c0982
Carlos Juárez <cjuarez@uca.edu.sv>
2025-08-21 12:11:26 -0600

    actualizado ejemplo 21/8

--- ejemplo0819_euler.py
+++ ejemplo0819_euler.py
@@ -35,10 +35,35 @@ v_euler[0] = v0  # Condición inicial
 # método de euler
 for i in range(N):
     v_euler[i+1] = v_euler[i] + h * (-B/M * v_euler[i])
-    print(v_euler)
+    #print(v_euler)
+
+# Método de Heun
+v_heun = np.zeros(N+1)
+v_heun[0] = v0
+# dv/dt = -B/M*v
+def f(v):
+    return -B/M*v
+for i in range(N):
+    vtemp = v_heun[i] + h*f(v_heun[i])
+    v_heun[i+1] = v_heun[i] + h*(f(v_heun[i])+f(vtemp))/2
+    print(v_heun)
+# Metódo RK2/3
+v_rk23 = np.zeros(N+1)
+v_rk23[0] = v0
+for i in range(N):
+    k1 = f(v_rk23[i])
+    vtemp = v_rk23[i] + 3/4*k1*h
+    k2 = f(vtemp)
+    v_rk23[i+1] = v_rk23[i] + h*(1/3*k1+2/3*k2)
+
+
+
+
 #Gráficos
 plt.plot(t, v_analitica, label='Analitica', color='blue')
 plt.plot(t, v_euler, label='Euler', color='red')
+plt.plot(t, v_heun, label='Heun', color='black')
+plt.plot(t, v_rk23, label='RK23', color='green')
 plt.xlabel('Tiempo (s)')
 plt.ylabel('Velocidad (m/s)')
 plt.title('Velocidad del sistema con friccion')
