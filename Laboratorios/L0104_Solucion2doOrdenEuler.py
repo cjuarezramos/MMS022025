8f6220c010b334f292e32b961828a1f978cbf618
Carlos Juárez <cjuarez@uca.edu.sv>
2025-08-22 13:25:48 -0600

    Actualización del archivo de euler.

---
+++ Laboratorios/L0104_Solucion2doOrdenEuler.py
@@ -0,0 +1,6 @@
+# Autor:
+# Fecha:
+# Descripción: Este programa resulve la ecuación diferencial que describe el sistema masa resorte amortiguador.
+
+
+# Los archivos de Python se almacenan como .py
