554c00b39a1efa04342cb8942232fc48d8a9b995
Carlos Juárez <cjuarez@uca.edu.sv>
2025-10-20 17:13:17 -0600

    Archivo de amortiguador

---
+++ Archivos Prueba/Amortiguador.py
@@ -0,0 +1,10 @@
+# Autor Carlos Juarez
+# Fecha 16/10/2025
+# Descripcion: Clase amortiguador
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+import control as ct
+import sympy as sp  
+
+sp.sym
