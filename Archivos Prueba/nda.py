1ed93eb0967e11d951bdcb23f0624a6bc2b2f44a
Carlos Juárez <cjuarez@uca.edu.sv>
2025-11-11 11:02:58 -0600

    laboratorio 4 fin

--- Archivos Prueba/nda.py
+++ Archivos Prueba/nda.py
@@ -1,9 +1,20 @@
 import control as ct
 import numpy as np
 import matplotlib.pyplot as plt
-K = 1
-B = 0.1
+from scipy.integrate import solve_ivp
+wn = 1000
+z = 0.1
+K=1
 J = 0.01
+B = 0.02
+def f(t,x):
+    U = 1
+    dx1dt = x[1]
+    dx2dt = -B/J*x[1] - K/J*x[0] + K/J*U
+    return np.array([dx1dt,dx2dt])
+sol = solve_ivp(f,[0,0.1],[0,0])
+plt.plot(sol.t,sol.y[0])
+plt.show()
 num_ol = [K]
 den_ol = [J, B, 0]
 G = ct.tf(num_ol, den_ol)
